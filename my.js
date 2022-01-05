2590db0bff9344523cda4ef5bb0e51f4b434eb50
Voyageson <2582376763@qq.com>
2022-01-06 01:16:43 +0800

    simplify inject-script and manifest

--- my.js
+++ my.js
@@ -1,21 +1,10 @@
-// 获取脚本内容并删除
-let scripts = document.querySelectorAll('script')
-let script = scripts[scripts.length-1]
-let text = script.text
-script.remove()
-
-// 删除“确认提交”内容
-text = text.replace(`if (!confirm("确认提交?")){`, '')
-text = text.replace(`return;`, '')
-text = text.replace(`}`, '')
-
-// 根据新内容，创建新脚本
+// inject script overload confirm
 let new_s = document.createElement('script')
 new_s.setAttribute('type', 'text/JavaScript')
-new_s.text = text
+new_s.text = 'window.confirm = (str)=>{return true}'
 document.body.appendChild(new_s)
 
-// 点击选项
+// choose options
 let values = document.querySelectorAll('input[value="2"]')
 for (i of values) {
     i.click()
